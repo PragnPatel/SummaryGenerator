0c13c910d6438d0f9ca0409e5b47b758b899795f
PragnPatel <49933906+PragnPatel@users.noreply.github.com>
2025-09-19 11:07:32 +0200

    app.py

--- app.py
+++ app.py
@@ -1,31 +1,48 @@
-from flask import Flask, request, jsonify, render_template
+# from flask import Flask, request, jsonify, render_template
+# from summarizer import *
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
+
+# @app.route('/summarize',methods=['POST'])
+# def summarize():
+    
+#     if request.method == 'POST':
+        
+#         text = request.form['originalText']
+#         if not request.form['numOfLines']:
+#             numOfLines = 3
+#         else:
+#             numOfLines = int(request.form['numOfLines'])
+            
+#         summary, original_length = generate_summary(text,numOfLines)
+        
+#         return render_template('result.html',
+#                                text_summary=summary,
+#                                lines_original = original_length,
+#                                lines_summary = numOfLines)
+    
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+import streamlit as st
 from summarizer import *
 
-app = Flask(__name__)
+st.title("Text Summarizer")
+input = st.text_area("Enter the text to summarize")
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/summarize',methods=['POST'])
-def summarize():
-    
-    if request.method == 'POST':
-        
-        text = request.form['originalText']
-        if not request.form['numOfLines']:
-            numOfLines = 3
-        else:
-            numOfLines = int(request.form['numOfLines'])
-            
-        summary, original_length = generate_summary(text,numOfLines)
-        
-        return render_template('result.html',
-                               text_summary=summary,
-                               lines_original = original_length,
-                               lines_summary = numOfLines)
-    
-    
-if __name__ == '__main__':
-    app.run(debug=True)
+if st.button("Summarize"):
+    if input.strip():
+        with st.spinner("Generating Sumary"):
+            summary, original_length = generate_summary(input, len(input))
+        st.subheader("Summary")
+        st.write(summary)
+        st.subheader("Original Length")
+        st.write(original_length)
+    else:
+        st.warning("Please enter some text to summarize")
 
